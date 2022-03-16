3e31eceaf06fbbfae5bac8f7292565254246bf7a
aprouco <adripenasrouco@gmail.com>
2022-03-16 17:26:11 +0100

    MediaConjuntoNúmeros

---
+++ session2.html.php
@@ -0,0 +1,59 @@
+<?php 
+    session_start();
+?>
+
+<!DOCTYPE html>
+
+<!--Escribe un programa que calcule la media de un conjunto de números positivos introducidos por
+teclado. A priori, el programa no sabe cuántos números se introducirán. El usuario indicará que ha
+terminado de introducir los datos cuando meta un número negativo.-->
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio10</title>
+</head>
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+    
+        <label for="numero">Introduce un número: </label>
+        <p><input type="number" name="numero"></p>
+        <input type="submit" value="Enviar">
+    </form>
+    <?php 
+        if(!isset($_SESSION['numeros'])){
+            $_SESSION['numeros'] = array();
+        }
+        if (isset($_POST['numero']) && filter_var($_POST['numero'],FILTER_VALIDATE_INT) && $_POST['numero'] >= 0) {
+            $n = $_POST['numero'];
+            array_push($_SESSION['numeros'],$n);
+        }elseif (isset($_POST['numero']) && filter_var($_POST['numero'],FILTER_VALIDATE_INT) && $_POST['numero'] < 0) {
+            $suma=0;
+            $divisor=count($_SESSION['numeros']);
+            for ($i=0; $i < $divisor; $i++) { 
+                $suma = $suma + $_SESSION['numeros'][$i];
+            }
+            $media = $suma / $divisor;
+            echo "La media es = <b>".$media."</b>";
+
+                #Destruimos la sesión#
+                $_SESSION = array();
+
+                if (ini_get("session.use_cookies")) {
+                $params = session_get_cookie_params();
+                setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+                );
+                }
+                // Finalmente, destruir la sesión.
+                session_destroy();
+
+        }else {
+            echo "<p>Introduce un número.</p>";
+        }
+    ?>
+</body>
+</html>
